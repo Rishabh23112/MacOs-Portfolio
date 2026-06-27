711f9be8a1aec188a9cf0cb9e15755a93ede757c
Rishabh23112 <irishabh.k2311@gmail.com>
2026-06-27 11:16:49 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "#components/Home"
+import Navbar from "#components/Navbar"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Home/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { navIcons, navLinks } from "#constants"
+import dayjs from "dayjs"
+
+const Navbar = () => {
+  return (
+    <nav className='fixed top-2 left-2 right-2 rounded-2xl'>
+        <div>
+            <img src="/images/logo.svg" alt="logo"/>
+            <p className='font-bold'>Rishabh's Portfolio</p>
+
+            <ul>
+                {navLinks.map(({id, name})=>(
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                {
+                    navIcons.map(({id, img})=>(
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} className="icon-hover" />
+                        </li>
+                    ))
+                }
+            </ul>
+            <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
